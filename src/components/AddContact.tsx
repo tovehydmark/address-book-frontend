40194aca8df4a34f45de44369f98a537007c81ac
tovehydmark <tovehydmark@gmail.com>
2022-05-12 23:07:02 +0200

    changed fetch from axios to regular fetch

--- src/components/AddContact.tsx
+++ src/components/AddContact.tsx
@@ -7,24 +7,26 @@ export function AddContact() {
   const [phone, setPhone] = useState("");
 
   function saveContact() {
-    console.log(userName);
-    console.log(email);
-    console.log(phone);
+    // console.log(userName);
+    // console.log(email);
+    // console.log(phone);
 
     //Fix new user object
     const newUser = { username: userName, email: email, phone: phone };
 
     //Posts the new contact to server
-    axios
-      .post("http://localhost:3000/addcontact", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newUser),
-      })
-      .then((response) => {
-        console.log(response.data);
+    //It works as it is sent to the back end but the result is blocked somehow on the way back
+
+    fetch("http://localhost:3000/users/addcontact", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
       });
   }
 
