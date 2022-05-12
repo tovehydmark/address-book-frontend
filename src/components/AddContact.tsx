a9812656d4c7f99ebc34cc7a3494b0f81b91948a
tovehydmark <tovehydmark@gmail.com>
2022-05-12 19:48:28 +0200

    form created for new user

--- src/components/AddContact.tsx
+++ src/components/AddContact.tsx
@@ -1,18 +1,48 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function AddContact() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
   function saveContact() {
-    console.log("contact saved");
+    console.log(userName);
+    console.log(email);
+    console.log(phone);
+
+    //Posts the new contact to server
+    axios.post("").then((response) => {
+      console.log(response.data);
+    });
   }
+
   return (
     <>
       <form action="">
         <label htmlFor="name">Namn: </label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          value={userName}
+          id="name"
+          onChange={(e) => setUserName(e.target.value)}
+        />
 
         <label htmlFor="email">Email: </label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          value={email}
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="number">Nummer: </label>
-        <input type="number" name="number" id="number" />
+        <input
+          type="number"
+          value={phone}
+          id="number"
+          onChange={(e) => setPhone(e.target.value)}
+        />
 
         <button onClick={saveContact}>Spara kontakt</button>
       </form>
