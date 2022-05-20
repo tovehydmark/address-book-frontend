a40db1c4865421fbaced1551e3ab033092f544e1
tovehydmark <tovehydmark@gmail.com>
2022-05-20 15:17:38 +0200

    general file for fetch

---
+++ src/components/fetch.js
@@ -0,0 +1,21 @@
+export async function makeRequest(url, method, body) {
+    try {
+        let response = await fetch(url, {
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        console.log(response);
+
+        if (response.status == 404) {
+            throw new Error("Not found")
+        }
+
+        return await response.json()
+    } catch (err) {
+        throw err
+    }
+}
