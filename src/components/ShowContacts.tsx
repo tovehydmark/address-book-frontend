b0c3c17706935d89f5a68600b50e121cc94fc81f
tovehydmark <tovehydmark@gmail.com>
2022-05-20 15:47:42 +0200

    fetches all contacts and prints them

--- src/components/ShowContacts.tsx
+++ src/components/ShowContacts.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { NewUser } from "../models/NewUser";
+import { makeRequest } from "./MakeRequest";
+
 export function ShowContacts() {
-  return (
-    <>
-      <p>hej</p>
-    </>
-  );
+  const [contacts, setContacts] = useState<NewUser[]>([]);
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  //Gets contacts from server
+  async function getContacts() {
+    let fetchContacts = await makeRequest(
+      "http://localhost:1337/users/addcontact",
+      "GET"
+    );
+    setContacts(fetchContacts);
+  }
+
+  //Prints contacts on webb page
+  let allContacts = contacts.map((contact: NewUser) => {
+    return (
+      <article key={contact.number}>
+        <h2>Namn: {contact.username}</h2>
+        <p>Email: {contact.email}</p>
+        <p>Telefon: {contact.number}</p>
+      </article>
+    );
+  });
+
+  return <>{allContacts}</>;
 }
